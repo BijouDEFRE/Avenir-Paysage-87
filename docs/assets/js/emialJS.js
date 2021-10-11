de438ba9de311a09d3a7a5a8209ea4711a5d8140
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-10-11 22:18:01 +0200

    update send mail

--- docs/assets/js/emialJS.js
+++ docs/assets/js/emialJS.js
@@ -1,5 +1,6 @@
 function sendMail () {
-    var tempParams = {
+
+    let tempParams = {
         from_name: document.getElementById("fromName").value,
         to_name: document.getElementById("toName").value,
         message: document.getElementById("msg").value,
@@ -8,5 +9,11 @@ function sendMail () {
     emailjs.send("service_8aiw497", "template_7cick6l", tempParams)
     .then(function (response) {
         console.log("success", response.status);
+        alert("le message a été envoyé avec succès");
     });
+
+    setTimeout(
+        document.querySelector(".contact-form").reset(),
+        1000
+    )
 }
